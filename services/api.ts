1e758ec9cafcacaad9005ae104d7bec4d153044e
warlleyrocha <warlleyrocha@icloud.com>
2025-12-27 21:39:39 -0300

    feat: add react-native-config para gestão de variavel de ambiente e atualiza URL base da API

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import Config from "react-native-config";
 
 export const api = axios.create({
-  baseURL: "http://10.0.2.2:3333",
+  baseURL: Config.API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
