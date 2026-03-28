2c6f7d1145a03c25839a7ff47e9ed4b4751a6005
warlleyrocha <warlleyrocha@icloud.com>
2026-03-28 18:09:55 -0300

    refactor: remove key redundante

--- src/features/republic/hooks/republic.keys.ts
+++ src/features/republic/hooks/republic.keys.ts
@@ -1,7 +1,6 @@
 export const republicKeys = {
   all: ["republics"] as const,
-  lists: () => [...republicKeys.all, "list"] as const,
-  list: () => [...republicKeys.lists()] as const,
+  list: () => [...republicKeys.all, "list"] as const,
   details: () => [...republicKeys.all, "detail"] as const,
   detail: (id: string) => [...republicKeys.details(), id] as const,
 };
